2d94b6e23b40785805a39d6a8844d4e1cdacbcf8
PhongTran <phong.tran@vina-payroll.com>
2024-07-20 08:43:44 +0700

    update

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\ProductByDayController;
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
@@ -32,5 +36,35 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
         Route::put('/users/{id}', [UserController::class, 'update']);
         Route::post('/users', [UserController::class, 'store']);
         Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+        // Category
+        Route::get('/categories', [CategoryController::class, 'index']);
+        Route::get('/categories/{id}', [CategoryController::class, 'show']);
+        Route::put('/categories/{id}', [CategoryController::class, 'update']);
+        Route::post('/categories', [CategoryController::class, 'store']);
+        Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+        // Product
+        Route::get('/products', [ProductController::class, 'index']);
+        Route::get('/getProducts', [ProductController::class, 'getProducts']);
+        Route::get('/products/{id}', [ProductController::class, 'show']);
+        Route::put('/products/{id}', [ProductController::class, 'update']);
+        Route::post('/products', [ProductController::class, 'store']);
+        Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+        // ProductByDay
+        Route::get('/product-by-days', [ProductByDayController::class, 'index']);
+        Route::get('/product-by-days/{id}', [ProductByDayController::class, 'show']);
+        Route::put('/product-by-days/{id}', [ProductByDayController::class, 'update']);
+        Route::post('/product-by-days', [ProductByDayController::class, 'store']);
+        Route::delete('/product-by-days/{id}', [ProductByDayController::class, 'destroy']);
+
+        // Order
+        Route::get('/orders', [OrderController::class, 'index']);
+        Route::get('/orders/{id}', [OrderController::class, 'show']);
+        Route::put('/orders/{id}', [OrderController::class, 'update']);
+        Route::post('/orders', [OrderController::class, 'store']);
+        Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
+
     });
 });
